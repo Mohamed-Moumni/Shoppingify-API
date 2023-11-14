b827fa4a69c0cdc403f309bade4b06b38d4c8f46
Mohamed-Moumni <mmoumni@student.1337.ma>
2023-11-14 16:15:45 +0000

    remove fontend folder

--- frontend/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type LogoAttributes = React.SVGProps<HTMLDivElement>;
-
-function Logo(props: LogoAttributes) {
-	return <div {...props}></div>;
-}
-
-export default Logo;
